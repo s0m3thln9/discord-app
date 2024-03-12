82ce8519759aed7b7ab30d40c6617c57c2034436
revus05 <skninni05@gmail.com>
2024-03-12 20:31:05 +0300

    react-select type error

--- src/components/Authorization/AuthorizationQRCode.tsx
+++ src/components/Authorization/AuthorizationQRCode.tsx
@@ -1,11 +1,9 @@
 const AuthorizationQRCode = () => {
 	return (
 		<div className={'grid place-items-center max-lg:hidden'}>
-			<img src="/public/img/qr-code.png" alt="" />
-			<h2 className={'mt-8 text-2xl font-semibold text-center text-[#f2f3f5]'}>
-				Log in with QR Code
-			</h2>
-			<p className={'mt-2 font-medium text-center text-[#b5bac1]'}>
+			<img src="/img/qr-code.png" alt="" />
+			<h2 className={'mt-8 text-center text-2xl font-semibold text-[#f2f3f5]'}>Log in with QR Code</h2>
+			<p className={'mt-2 text-center font-medium text-[#b5bac1]'}>
 				Scan this with the <strong>Discord mobile app</strong> to log in instantly.
 			</p>
 		</div>
