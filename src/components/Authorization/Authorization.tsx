c51fbcd3b470c0d3394ac60ecf8396f370b69aa9
s0m3thln9 <dunlock67@gmail.com>
2024-03-13 09:36:34 +0300

    fix images

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -2,7 +2,7 @@ import AuthorizationMain from './AuthorizationMain.tsx'
 
 const Authorization = () => {
 	return (
-		<div className={'w-full select-none h-svh bg-[url("/img/auth-background.jpg")] bg-no-repeat bg-cover grid place-content-center'}>
+		<div className={`w-full select-none h-svh bg-auth-and-reg-background bg-no-repeat bg-cover grid place-content-center`}>
 			<AuthorizationMain />
 		</div>
 	)
--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -2,7 +2,7 @@ import RegistrationMain from './RegistrationMain.tsx'
 
 const Registration = () => {
 	return (
-		<div className={'w-full h-svh select-none bg-[url("/img/auth-background.jpg")] bg-cover bg-no-repeat grid place-content-center'}>
+		<div className={'w-full h-svh select-none bg-auth-and-reg-background bg-cover bg-no-repeat grid place-content-center'}>
 			<RegistrationMain />
 		</div>
 	)
