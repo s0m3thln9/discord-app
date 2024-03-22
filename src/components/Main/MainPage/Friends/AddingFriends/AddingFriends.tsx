5544b9d55d3bfa544c6624042448fd60ee912477
revus05 <skninni05@gmail.com>
2024-03-22 16:55:09 +0300

    friend image

--- src/components/Main/MainPage/Friends/AddingFriends/AddingFriends.tsx
+++ src/components/Main/MainPage/Friends/AddingFriends/AddingFriends.tsx
@@ -4,56 +4,63 @@ import { useSendFriendRequestMutation } from '../../../../../api/api.ts'
 import { SendFriendRequestResponse } from '../../../../../types/friends.ts'
 import { twMerge } from 'tailwind-merge'
 import { clsx } from 'clsx'
+import addFriendImage from '../../../../../assets/img/addFriends.svg'
 
 const AddingFriends = () => {
 	const [username, setUsername] = useState('')
 	const [sendFriendRequest] = useSendFriendRequestMutation()
 	const [serverResponse, setServerResponse] = useState<SendFriendRequestResponse | null>(null)
 	return (
-		<div className={'h-fit grow border-b-[1px] border-b-[#3f4147] px-[1.875rem] py-5'}>
-			<h2 className={'uppercase text-white'}>Add friends</h2>
-			<p className={'mt-2 text-sm'}>You can add friends with their Discord username.</p>
-			<div
-				className={twMerge(
-					clsx(
-						`mt-4 flex h-12 items-center rounded-lg border-[1px] bg-[#1e1f22] px-3 focus-within:border-[#00a7fb]`,
-						serverResponse?.success
-							? 'border-[#23a55a] focus-within:border-[#23a55a]'
-							: 'border-[#e93b42] focus-within:border-[#e93b42]',
-						serverResponse === null && 'border-[transparent] focus-within:border-[#00a7fb]',
-					),
-				)}
-			>
-				<div className={'mr-3 grow'}>
-					<input
-						type="text"
-						placeholder={'You can add friends with their Discord username.'}
-						className={'w-full bg-[transparent] text-white'}
-						value={username}
-						onChange={e => {
-							setUsername(e.target.value)
-							setServerResponse(null)
-						}}
-					/>
-				</div>
-				<Button
-					variant={'primary'}
-					className={'h-8 w-fit'}
-					disabled={username === ''}
-					onClick={async () => {
-						const response = await sendFriendRequest({ username }).unwrap()
-						setServerResponse(response)
-						console.log(response)
-					}}
+		<div className={'flex grow flex-col'}>
+			<div className={'h-fit border-b-[1px] border-b-[#3f4147] px-[1.875rem] py-5'}>
+				<h2 className={'uppercase text-white'}>Add friends</h2>
+				<p className={'mt-2 text-sm'}>You can add friends with their Discord username.</p>
+				<div
+					className={twMerge(
+						clsx(
+							`mt-4 flex h-12 items-center rounded-lg border-[1px] bg-[#1e1f22] px-3 focus-within:border-[#00a7fb]`,
+							serverResponse?.success
+								? 'border-[#23a55a] focus-within:border-[#23a55a]'
+								: 'border-[#e93b42] focus-within:border-[#e93b42]',
+							serverResponse === null && 'border-[transparent] focus-within:border-[#00a7fb]',
+						),
+					)}
 				>
-					Send Friend Request
-				</Button>
+					<div className={'mr-3 grow'}>
+						<input
+							type="text"
+							placeholder={'You can add friends with their Discord username.'}
+							className={'w-full bg-[transparent] text-white'}
+							value={username}
+							onChange={e => {
+								setUsername(e.target.value)
+								setServerResponse(null)
+							}}
+						/>
+					</div>
+					<Button
+						variant={'primary'}
+						className={'h-8 w-fit'}
+						disabled={username === ''}
+						onClick={async () => {
+							const response = await sendFriendRequest({ username }).unwrap()
+							setServerResponse(response)
+							console.log(response)
+						}}
+					>
+						Send Friend Request
+					</Button>
+				</div>
+				{serverResponse && (
+					<p className={twMerge(clsx('mt-2'), serverResponse.success ? 'text-[#2dc770]' : 'text-[#e93b42]')}>
+						{serverResponse.message}
+					</p>
+				)}
 			</div>
-			{serverResponse && (
-				<p className={twMerge(clsx('mt-2'), serverResponse.success ? 'text-[#2dc770]' : 'text-[#e93b42]')}>
-					{serverResponse.message}
-				</p>
-			)}
+			<div className={'flex grow flex-col items-center justify-center'}>
+				<img src={addFriendImage} alt="addFriend" />
+				<p className={'mt-8 text-[#949ba4]'}>Wumpus is waiting on friends. You don’t have to though!</p>
+			</div>
 		</div>
 	)
 }
